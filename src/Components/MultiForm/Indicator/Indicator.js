57388eee25dc324b704ca8da30b2f97b0f60c858
jeanlouisHERVE <jeanlouis.herve@hotmail.fr>
2022-03-02 08:15:17 +0100

    add Indicator component

--- src/Components/MultiForm/Indicator/Indicator.js
+++ src/Components/MultiForm/Indicator/Indicator.js
@@ -6,11 +6,27 @@ import diet from './icons/diet.svg';
 import allergie from './icons/allergy.svg';
 import thumb from './icons/thumb.svg';
 
-export default function Indicator() {
+export default function Indicator({formIndex}) {
+
+
+
   return (
     <div className='container-indicator'>
         <div className='lines-container'>
-            <div className='line upper-line'></div>
+            <div 
+                className='line upper-line'
+                style={{
+                    width: formIndex === 1 ? "0%"
+                        : formIndex === 2 ? "0%" 
+                        : formIndex === 3 ? "25%"  
+                        : formIndex === 4 ? "50%"  
+                        : formIndex === 5 ? "75%"    
+                        : formIndex === 6 ? "100%"
+                        : ""  
+            }}>
+                
+
+            </div>
             <div className='line under-line'></div>       
         </div>
         <div className='container-img'>
--- src/Components/MultiForm/MultiForm.js
+++ src/Components/MultiForm/MultiForm.js
@@ -28,16 +28,34 @@ export default function MultiForm() {
             setAllFormData(newData)
         }
     }
+
+    //
+    const elements = [
+        <CardBegin key="1" modifyIndex={modifyIndex} />,
+        <DietForm key="2" modifyIndex={modifyIndex} />,
+        <foodStyle  key="3"modifyIndex={modifyIndex} />,
+        <Allergies key="4"modifyIndex={modifyIndex} />,
+        <HateLove key="5"modifyIndex={modifyIndex} />,
+        <CardEnd key="6" modifyIndex={modifyIndex} />,
+    ]
  
   return (
     <div className='container-multiform'>
-        <Indicator />
-        {formIndex === 1 ? <CardBegin modifyIndex={modifyIndex} /> 
+        <Indicator formIndex={formIndex}/>
+
+        {elements.map((item, index) => {
+            if((index+1) === formIndex){
+            return elements[index]
+            }
+        })}
+
+        {/* {formIndex === 1 ? <CardBegin modifyIndex={modifyIndex} /> 
         : formIndex === 2 ? <DietForm modifyIndex={modifyIndex} />
         : formIndex === 3 ? <FoodStyleForm modifyIndex={modifyIndex} />
         : formIndex === 4 ? <Allergies modifyIndex={modifyIndex} />
         : formIndex === 5 ? <HateLove modifyIndex={modifyIndex} />
-        : ""} 
+        : formIndex === 6 ? <CardEnd modifyIndex={modifyIndex} />
+        : ""}  */}
     </div>
   )
 }
